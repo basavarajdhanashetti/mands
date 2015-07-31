fe93d6285e101f113496bcb85afe5ca89cb49ac2
basavarajdhanashetti <basavarajdhanashetti@gmail.com>
2015-07-31 15:38:07 +0530

    updated code
    updated code for maven plugin

--- json-formatter/apiproxy/resources/node/json-formatter.js
+++ json-formatter/apiproxy/resources/node/json-formatter.js
@@ -1,30 +1,16 @@
 var http = require("http");
 
+var jsonFormatter = require("./transfromation-lib.js");
+
 http.createServer(function (req, res) {
   var body = "";
   req.on('data', function (chunk) {
     body += chunk;
   });
   req.on('end', function () {
-    var jsonObj = JSON.parse(body);
-
-    var newJsonObj =  JSON.stringify({ 
-        details:{ 
-        	desc: 'General Details',
-            name: jsonObj.name,
-            age: jsonObj.age
-        }, 
-        adress: {
-            line1: jsonObj.line1,
-            line2: jsonObj.line2,
-            city: jsonObj.city
-        }, 
-        country: jsonObj.country
-      });
-    
     res.writeHead(200, {"Content-Type": "application/json"});
-   
-    res.end(newJsonObj);
+    res.end(jsonFormatter.buildClientJSON(body));
+
   });
 }).listen(8080);
 
